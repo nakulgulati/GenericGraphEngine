644e80bb3f55c0140e3a5156b7221babf41588f1
simar <simar.arneja17@gmail.com>
2015-02-28 00:07:52 +0530

    Server Socket

--- GraphEngine_Backend/src/main/java/Test.java
+++ GraphEngine_Backend/src/main/java/Test.java
@@ -1,16 +1,43 @@
 import org.javalite.activejdbc.Base;
 
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.*;
 import java.util.List;
 
 public class Test {
 
+    private static ServerSocket ss = null;
+    private static Socket cs = null;
+    private static DataInputStream is = null;
     public static void main(String[] args){
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/graph_engine_db", "root", "");
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/graph_engine_db", "root", "1234");
+
+        List<String> list = Node.attributes();
+        String s;
+
+        for(int i = 0; i<list.size(); i++){
+            s = list.get(i);
+            System.out.println(s);
+        }
+
+        Node n = new Node();
+
+        n.set("name", "A");
+
+    try{
+        ss = new ServerSocket(6969);
 
 
-        Node n = Node.findById(1);
+        while(true){
+            cs = ss.accept();
+            is = new DataInputStream(cs.getInputStream());
+            System.out.println(is.readUTF());
 
-        System.out.println(n.toJson(true));
+        }
+    }catch (Exception e){
+        System.err.println(e);
+    }
 
     }
 
