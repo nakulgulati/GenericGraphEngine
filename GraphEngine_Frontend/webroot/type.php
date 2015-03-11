e42bff0d0a05c23716f9afe580f861b0916ec302
Lalit-20 <lalitahuja.20@gmail.com>
2015-03-11 17:23:36 +0530

    Added Type form

---
+++ GraphEngine_Frontend/webroot/type.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Type Form</title>
+</head>
+<body>
+
+<Form name ="type" Method ="POST" Action ="index.php">
+
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="name">
+    <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Submit">
+
+</FORM>
+
+</body>
+</html>
