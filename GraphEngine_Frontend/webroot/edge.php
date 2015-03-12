4ed17353847eff8a57a98d57aaa15f9b81e2aff3
Lalit-20 <lalitahuja.20@gmail.com>
2015-03-12 17:15:44 +0530

    Added edge form

---
+++ GraphEngine_Frontend/webroot/edge.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title>Type Form</title>
+</head>
+<body>
+
+<Form name ="edge_add" Method ="POST" Action ="test.php">
+
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="from_id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="to_id">
+    <INPUT TYPE = "Submit" Name = "Submit_add" VALUE = "Submit">
+
+</FORM>
+<Form name ="edge_delete" Method ="POST" Action ="index.php">
+
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="from_id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="to_id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="to_id">
+    <INPUT TYPE = "Submit" Name = "Submit_delete" VALUE = "Submit">
+
+</FORM>
+<Form name ="edge_update" Method ="POST" Action ="index.php">
+
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="from_id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="to_id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="to_id">
+    <INPUT TYPE = "Submit" Name = "Submit_update" VALUE = "Submit">
+
+</FORM>
+<Form name ="edge_read" Method ="POST" Action ="index.php">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="from_id">
+    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="field">
+
+    <INPUT TYPE = "Submit" Name = "Submit_read" VALUE = "Submit">
+
+</FORM>
+
+</body>
+</html>
