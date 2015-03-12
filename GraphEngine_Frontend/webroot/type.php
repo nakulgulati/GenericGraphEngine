d60103074b98b2ce02976db00dad0511a863266c
Lalit-20 <lalitahuja.20@gmail.com>
2015-03-13 03:57:10 +0530

    made changes in type form

--- GraphEngine_Frontend/webroot/type.php
+++ GraphEngine_Frontend/webroot/type.php
@@ -6,7 +6,7 @@
 
 <Form name ="type_add" Method ="POST" Action ="test.php">
 
-    <INPUT TYPE = "TEXT" VALUE ="Type Name" Name ="name">
+    <INPUT TYPE = "TEXT" VALUE ="name" Name ="name">
 
     <INPUT TYPE = "Submit" Name = "Submit_type_add" VALUE = "Submit">
 
@@ -14,19 +14,19 @@
 
 <Form name ="type_delete" Method ="POST" Action ="test.php">
 
-    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="id">
+    <INPUT TYPE = "TEXT" VALUE ="id" Name ="id">
     <INPUT TYPE = "Submit" Name = "Submit_type_delete" VALUE = "Submit">
 
 </FORM>
 <Form name ="type_update" Method ="POST" Action ="test.php">
-    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="id">
-    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="name">
+    <INPUT TYPE = "TEXT" VALUE ="id" Name ="id">
+    <INPUT TYPE = "TEXT" VALUE ="name" Name ="name">
     <INPUT TYPE = "Submit" Name = "Submit_type_update" VALUE = "Submit">
 
 </FORM>
 <Form name ="type_read" Method ="POST" Action ="test.php">
 
-    <INPUT TYPE = "TEXT" VALUE ="Type" Name ="field">
+    <INPUT TYPE = "TEXT" VALUE ="field" Name ="field">
 
     <INPUT TYPE = "Submit" Name = "Submit_type_read" VALUE = "Submit">
 
