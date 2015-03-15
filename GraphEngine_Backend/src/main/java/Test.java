d534bdc864a22d03d7e4a28a9803e7c1ec6101d9
Nakul Gulati <nakgulati@gmail.com>
2015-03-16 04:26:07 +0530

    Update Dependencies Refactored response Tested Type-add

--- GraphEngine_Backend/src/main/java/Test.java
+++ GraphEngine_Backend/src/main/java/Test.java
@@ -5,18 +5,20 @@ public class Test {
     public static void main(String[] args){
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/graph_engine_db", "root", "");
 
+        /*Type type = new Type();
+        type.set("name", "K");
+        type.saveIt();*/
+
         String s = "{\n" +
                 "\"table\" : \"type\",\n" +
-                "\"operation\" : \"read\",\n" +
-                "\"field\" : \"*\"\n" +
+                "\"operation\" : \"create\",\n" +
+                "\"data\" : {" +
+                    "name : L" +
+                    "}" +
                 "}";
 
         String response = new Process(s).operateCRUD();
         System.out.println(response);
 
-    }
-
-    public synchronized void t(){
-        System.out.println();
     }
 }
