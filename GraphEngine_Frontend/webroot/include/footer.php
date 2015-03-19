5ea4b81674a166ea4ab1f5103838ab9eb91a729a
Lalit-20 <lalitahuja.20@gmail.com>
2015-03-20 03:46:05 +0530

    Main Page

--- GraphEngine_Frontend/webroot/include/footer.php
+++ GraphEngine_Frontend/webroot/include/footer.php
@@ -2,5 +2,32 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
+<script language="JavaScript">
+
+    function onStart(){
+        $("form").hide();
+    }
+    $(document).ready(onStart());
+
+   /* var menu=document.querySelector(".master")
+    menu.addEventListener("click",disappear,false);*/
+
+    $("li.master").find("ul").find("li").click(disappear);
+
+    function disappear(e)
+    {
+        if (e.target !== e.currentTarget) {
+            var li = $(this).attr("value");
+            if(li == 'type_add'){
+                console.log("true");
+            }
+            //console.log($("#type_add"));
+            $("form[name=li]").fadeIn();
+        }
+        e.stopPropagation();
+    }
+
+</script>
+
 </body>
 </html>
--- GraphEngine_Frontend/webroot/index.php
+++ GraphEngine_Frontend/webroot/index.php
@@ -8,21 +8,21 @@
 
                 <ul class="nav nav-pills nav-stacked">
 
-                    <li><a data-toggle="collapse" href="#type" aria-expanded="false" aria-controls="type" >Type</a>
+                    <li class="master"><a data-toggle="collapse" href="#type" aria-expanded="false" aria-controls="type">Type</a>
                         <div class="collapse" id="type">
                             <div class="well">
                                 <ul >
-                                    <li><a href="#">Add</a></li>
-                                    <li><a href="#">Delete</a></li>
-                                    <li><a href="#">Update</a></li>
+                                    <li value="type_add"><a href="#">Add</a></li>
+                                    <li value="type_delete"><a href="#">Delete</a></li>
+                                    <li ><a href="#">Update</a></li>
                                     <li><a href="#">Read</a></li>
                                 </ul>
                             </div>
                         </div></li>
-                    <li><a data-toggle="collapse" href="#node" aria-expanded="false" aria-controls="node" >Node</a>
+                    <li class="master"><a data-toggle="collapse" href="#node" aria-expanded="false" aria-controls="node">Node</a>
                         <div class="collapse" id="node">
                             <div class="well">
-                                <ul >
+                                <ul>
                                     <li><a href="#">Add</a></li>
                                     <li><a href="#">Delete</a></li>
                                     <li><a href="#">Update</a></li>
@@ -31,7 +31,7 @@
                             </div>
                         </div>
                     </li>
-                    <li><a data-toggle="collapse" href="#edge" aria-expanded="false" aria-controls="edge" >Edge</a>
+                    <li class="master"><a data-toggle="collapse" href="#edge" aria-expanded="false" aria-controls="edge">Edge</a>
                         <div class="collapse" id="edge">
                             <div class="well">
                                 <ul >
