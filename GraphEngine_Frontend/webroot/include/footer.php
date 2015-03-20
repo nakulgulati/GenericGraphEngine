f0728ccf312f7b2bd2c0f65fc5f488f367ee73e8
Nakul Gulati <nakgulati@gmail.com>
2015-03-21 03:48:54 +0530

    Frontend Container for graph

--- GraphEngine_Frontend/webroot/include/footer.php
+++ GraphEngine_Frontend/webroot/include/footer.php
@@ -1,7 +1,3 @@
-<div id="sigmaTest">
-
-</div>
-
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
--- GraphEngine_Frontend/webroot/include/header.php
+++ GraphEngine_Frontend/webroot/include/header.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,14 +17,6 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-    <style type="text/css">
-        #sigmaTest {
-            max-width: 400px;
-            height: 400px;
-            margin: auto;
-        }
-    </style>
 </head>
 
 <body>
