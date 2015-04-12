3708727a242d3647e99d2b3f6b6f5e40bd3c293b
Nakul Gulati <nakgulati@gmail.com>
2015-04-12 06:15:46 +0530

    Test Class Stubs

---
+++ GraphEngine_Backend/src/test/java/EdgeTest.java
@@ -0,0 +1,42 @@
+import org.junit.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EdgeTest{
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @After
+    public void tearDown() throws Exception{
+
+    }
+
+    @org.junit.Test
+    public void testRead() throws Exception{
+
+    }
+
+    @Test
+    public void testAdd() throws Exception{
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception{
+
+    }
+
+    @Test
+    public void testRemove() throws Exception{
+
+    }
+
+    @Test
+    public void testGetEntityAssociations() throws Exception{
+
+    }
+}
---
+++ GraphEngine_Backend/src/test/java/NodeTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NodeTest{
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @After
+    public void tearDown() throws Exception{
+
+    }
+
+    @org.junit.Test
+    public void testRead() throws Exception{
+
+    }
+
+    @Test
+    public void testAdd() throws Exception{
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception{
+
+    }
+
+    @Test
+    public void testRemove() throws Exception{
+
+    }
+}
